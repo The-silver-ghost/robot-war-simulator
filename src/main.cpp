86c43763c5989314f903081815e2d4f1cf8edd7c
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-05-01 18:22:24 +0800

    extract data from config file

--- src/main.cpp
+++ src/main.cpp
@@ -21,3 +21,35 @@ Phone:
 Lecture Section: TC3L
 Tutorial Section: T12L
 **********|**********|**********/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class Battlefield{
+};
+
+int main(){
+    ifstream configFile("config.cfg");
+    string line;
+    int row,col,numberOfSteps,numberOfRobots;
+    while(getline(configFile,line)){
+        if (line.find("M by N") != string::npos){
+            string coordinates = line.substr(line.find(':')+2);
+            row = stoi(coordinates.substr(0,coordinates.find(' ')));
+            col = stoi(coordinates.substr(coordinates.find(' ')+1,-1));
+        }
+        else if (line.find("steps:") != string::npos){
+            numberOfSteps = stoi(line.substr(line.find(':') +2,-1));
+        }
+        else if (line.find("robots") != string::npos){
+            numberOfRobots = stoi(line.substr(line.find(':')+2,-1));
+        }
+        else if (line.find("Generic") != string::npos){
+            cout << line << endl;
+        }
+    }
+    return 0;
+}

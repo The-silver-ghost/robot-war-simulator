4a9abe258351f96a93a8ce7cdd63b433da6fcab7
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-05-04 13:18:10 +0800

    add ability to insert robots in battlefield

--- src/main.cpp
+++ src/main.cpp
@@ -38,28 +38,32 @@ class Robot{
         string robotName;
         char robotSymbol;
     public:
-        virtual void see(int,int) = 0;
+        /*virtual void see(int,int) = 0;
         virtual void move() = 0;
         virtual void shoot(int,int) = 0;
-        virtual void think() = 0;  //to be changed in derived classes
+        virtual void think() = 0;  //to be changed in derived classes*/ //commented for testing purposes
 
         void setPosX(int x){robotPosX=x;}
         void setPosY(int y){robotPosY=y;}
         void setRobotName(string n){robotName=n;}
         void setRobotSymbol(char c){robotSymbol=c;}
         void setKills(int k){robotKills=k;}
+        int getPosX(){return robotPosX;}
+        int getPosY(){return robotPosY;}
+        char getrobotSymbol(){return robotSymbol;}
 };
 
 class Battlefield{
     private:
         int row,col,steps;
-        vector<char> field;
+        vector<Robot*> robots;
     public:
         Battlefield(int,int,int);
         int getRow(){return row;}
         int getCol(){return col;}
         int getSteps(){return steps;}
         void displayBattlefield();
+        void addRobot(Robot* robot);
 };
 
 Battlefield::Battlefield(int r, int c, int s){
@@ -68,18 +72,48 @@ Battlefield::Battlefield(int r, int c, int s){
     steps=s;
 }
 
+void Battlefield::addRobot(Robot* robot){
+    robots.push_back(robot);
+}
+
 void Battlefield::displayBattlefield(){
     for (int i=0;i<row;i++){
         cout << endl;
         for (int j=0;j<col;j++){
-            cout << '.';
+            bool robotFound = false;
+            for (Robot* activeBots: robots){
+                if (activeBots->getPosX()==j && activeBots->getPosY()==i){
+                    cout << activeBots->getrobotSymbol();
+                    robotFound = true;
+                    break;
+                }
+            }
+            if (!robotFound){
+                cout << '.';
+            }
         }
     }
     cout << endl;
 }
 
 int main(){
-    Battlefield b(3,15,2);
+    Battlefield b(10,10,2);
+    Robot r;
+    Robot r2;
+    Robot r3;
+    r.setPosX(9);
+    r.setPosY(1);
+    r.setRobotSymbol('r');
+    r2.setPosX(2);
+    r2.setPosY(5);
+    r2.setRobotSymbol('e');
+    r3.setPosX(6);
+    r3.setPosY(7);
+    r3.setRobotSymbol('d');
+
+    b.addRobot(&r);
+    b.addRobot(&r2);
+    b.addRobot(&r3);
     b.displayBattlefield();
     return 0;
 }
